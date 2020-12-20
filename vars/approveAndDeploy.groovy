eac38bcc004f3db7179612e78dc71a0b16075cbf
Votre Nom <ngomadean@gmail.com>
2020-12-20 01:39:16 +0100

    m

--- vars/approveAndDeploy.groovy
+++ vars/approveAndDeploy.groovy
@@ -1,12 +1,6 @@
 #!/usr/bin/env groovy
 
 def call(String dockerImageToDeploy) {
-  stage('Waiting Approval') {
-    timeout(time:1, unit:'DAYS') {
-      input message: "Is it ok to deploy ${dockerImageToDeploy}?",
-        ok: 'Deploy'
-      }
-    }
   stage('Deploy') {
     build job: 'demoapp-staging-deployer',
       parameters: [string(name: 'DOCKER_IMAGE',
